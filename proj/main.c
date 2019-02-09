eead4b300581171489fc94fae5297c0bef26cb6c
pramukap <pramuka.perera@gmail.com>
2019-02-09 16:41:43 -0600

    Added section in startup_gcc.c for custom interrupt handlers

--- proj/main.c
+++ proj/main.c
@@ -1,54 +1,14 @@
-//*****************************************************************************
-//
-// project0.c - Example to demonstrate minimal TivaWare setup
-//
-// Copyright (c) 2012-2017 Texas Instruments Incorporated.  All rights reserved.
-// Software License Agreement
-// 
-// Texas Instruments (TI) is supplying this software for use solely and
-// exclusively on TI's microcontroller products. The software is owned by
-// TI and/or its suppliers, and is protected under applicable copyright
-// laws. You may not combine this software with "viral" open-source
-// software in order to form a larger program.
-// 
-// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
-// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
-// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
-// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
-// DAMAGES, FOR ANY REASON WHATSOEVER.
-// 
-// This is part of revision 2.1.4.178 of the EK-TM4C123GXL Firmware Package.
-//
-//*****************************************************************************
-
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/hw_types.h"
-#include "inc/hw_memmap.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/gpio.h"
+#include "inc/tm4c123gh6pm.h"
+#include "driverlib/pll.h"
+//#include "inc/pll.h"
 
-//*****************************************************************************
-//
-// Define pin to LED color mapping.
-//
-//*****************************************************************************
+//#include "inc/hw_types.h"
+//#include "inc/hw_memmap.h"
+//#include "driverlib/sysctl.h"
+//#include "driverlib/gpio.h"
 
-//*****************************************************************************
-//
-//! \addtogroup example_list
-//! <h1>Project Zero (project0)</h1>
-//!
-//! This example demonstrates the use of TivaWare to setup the clocks and
-//! toggle GPIO pins to make the LED's blink. This is a good place to start
-//! understanding your launchpad and the tools that can be used to program it.
-//
-//*****************************************************************************
-
-#define RED_LED   GPIO_PIN_1
-#define BLUE_LED  GPIO_PIN_2
-#define GREEN_LED GPIO_PIN_3
 
 //*****************************************************************************
 //
@@ -56,62 +16,30 @@
 //
 //*****************************************************************************
 #ifdef DEBUG
-void
-__error__(char *pcFilename, uint32_t ui32Line)
-{
+void __error__(char *pcFilename, uint32_t ui32Line) {
 }
 #endif
 
-//*****************************************************************************
-//
-// Main 'C' Language entry point.  Toggle an LED using TivaWare.
-//
-//*****************************************************************************
-int
-main(void)
-{
-    //
-    // Setup the system clock to run at 50 Mhz from PLL with crystal reference
-    //
-    SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|
-                    SYSCTL_OSC_MAIN);
+void Heartbeat_Init (void) {
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	while ((SYSCTL_RCGCGPIO_R | 0x20) == 0);	
+	
+	GPIO_PORTF_AFSEL_R &= ~0x0E;
+	GPIO_PORTF_AMSEL_R &= ~0x0E; 
+	GPIO_PORTF_DEN_R |= 0x0E;
+	GPIO_PORTF_DIR_R |= 0x0E; 
+	GPIO_PORTF_DATA_R &= ~0x0E;
+}
 
-    //
-    // Enable and wait for the port to be ready for access
-    //
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-    {
-    }
-    
-    //
-    // Configure the GPIO port for the LED operation.
-    //
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED_LED|BLUE_LED|GREEN_LED);
+int main (void) {
+	PLL_Init (Bus80_000MHz);	
+	Heartbeat_Init();
+	
+	while (1) {
+		GPIO_PORTF_DATA_R ^= 0x0E;
 
-    //
-    // Loop Forever
-    //
-    while(1)
-    {
-        //
-        // Turn on the LED
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, RED_LED|BLUE_LED|GREEN_LED, RED_LED);
-
-        //
-        // Delay for a bit
-        //
-        SysCtlDelay(2000000);
-
-        //
-        // Turn on the LED
-        //
-        GPIOPinWrite(GPIO_PORTF_BASE, RED_LED|BLUE_LED|GREEN_LED, BLUE_LED);
-
-        //
-        // Delay for a bit
-        //
-        SysCtlDelay(2000000);
-    }
+		for (int i = 0; i < 10000000; i++) {
+			__asm__("");	//prevents delay loop from being optimized out
+		}
+	}
 }
